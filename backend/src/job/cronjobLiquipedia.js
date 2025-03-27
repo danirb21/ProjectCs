866779d53fda52d14d96f2fb39910dc7a414bd8a
DanielRB21 <danielsolisalf@gmail.com>
2025-03-27 19:02:17 +0100

    27/03

--- backend/src/job/cronjobLiquipedia.js
+++ backend/src/job/cronjobLiquipedia.js
@@ -1,31 +1,38 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
-const torneos=require("../models/event.js")
+const Torneo=require("../models/event.js")
 const axios = require("axios");
 const cron = require("node-cron");
 const conectarMysql=require("../config/mysqlConnection");
+const { get } = require("http");
 
-async function getEvents(startdate, enddate) {
+async function getEvents(startdate) {
     try {
-        const json = await axios.get("URL_DE_LA_API_DE_LIQUIPEDIA"); // Reemplazar con la URL real
-        return response.data;
+        const json = await axios.get("URL_DE_LA_API_DE_LIQUIPEDIA").then(resp =>{
+            const eventsJson=JSON.parse(resp.data);
+            let eventos=[];
+            eventsJson.array.forEach(element => {
+                const torneo=new Torneo(element.pageid, element.name,element.prizepool,element.lan)
+                eventos.push(torneo);
+            });
+        }); // Reemplazar con la URL real
+        return eventos;
     } catch (error) {
         console.error("Error obteniendo datos de Liquipedia", error);
         return [];
     }
 }
 
-/*
-async function actualizarTorneos() {
+async function actualizarTorneos(startdate) {
     const connection = await conectarMysql();
-    torneos = await obtenerTorneosDeLiquipedia();
+    const torneos = await getEvents(startdate);
     
     for (const torneo of torneos) {
         const { event_id, event_name, prize_pool, lan } = torneo;
         
         try {
             await connection.execute(
-                "INSERT INTO torneos (event_id, event_name, prize_pool, lan) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE event_name = VALUES(event_name), prize_pool = VALUES(prize_pool), lan = VALUES(lan)",
+                //"INSERT INTO torneos (event_id, event_name, prize_pool, lan) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE event_name = VALUES(event_name), prize_pool = VALUES(prize_pool), lan = VALUES(lan)",
                 [event_id, event_name, prize_pool, lan]
             );
             console.log(`Torneo ${event_name} actualizado.`);
@@ -36,7 +43,7 @@ async function actualizarTorneos() {
     
     await connection.end();
 }
-
+/*
 // Ejecutar cada 24 horas
 cron.schedule("0 0 * * *", () => {
     console.log("Ejecutando cron job para actualizar torneos");
